4b74655d0fb1db86f0d94186b09cd14868d48341
Cavan Theiss <cavanrt@gmail.com>
2025-10-11 23:12:03 -0500

    Update CoreLoop.cpp

--- CoreLoop.cpp
+++ CoreLoop.cpp
@@ -2,3 +2,9 @@
 // update game logic
 // render frame
 // I'm gonna copy down some example code
+
+#include <chrono> // for time management
+#include <thread> // For sleeping to control frame rate
+#include <iostream> // For basic output
+
+//
